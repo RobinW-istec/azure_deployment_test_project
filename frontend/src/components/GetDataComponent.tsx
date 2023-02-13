e1f1e2b964bdcd322cfffe05e17a91bc30c82c59
RW <rw@istec.de>
2023-02-13 19:19:02 +0100

    buttons zum Ändern der Api Abfrage, Ergebnis in der Konsole sichtbar, ist alles nur zum rumprobieren für die deployte Version

--- frontend/src/components/GetDataComponent.tsx
+++ frontend/src/components/GetDataComponent.tsx
@@ -1,16 +1,25 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 
+type DataType = {
+    id: bigint,
+    name: string,
+    devMode: boolean,
+    someNumber: bigint
+}
+
 export const GetDataComponent = () => {
-    const [data, setData] = useState();
+    const [data, setData] = useState<DataType[]>();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState();
+    const [value, setValue] = useState<String>("");
 
     useEffect(() => {
-        axios.get('http://localhost:8080/api/data', {
-            headers: {'Access-Control-Allow-Origin': '*'}
+        axios.get('http://localhost:8080/api/data' + value, {
+            // headers: {'Access-Control-Allow-Origin': '*'}
         })
             .then(response => setData(response.data))
+            .then(() => console.log(data))
             .catch(error => {
                 console.log("Error fetching: " , error);
                 setError(error);
@@ -18,14 +27,32 @@ export const GetDataComponent = () => {
             .finally(() => {
                 setLoading(false);
             })
-    },[])
+    },[value])
 
-    if(error) return (<>Error!</>);
+    if(error || data == null) return (<>Error!</>);
     if(loading) return (<>Loading...</>);
 
+    function changeDataOutput(option : string) {
+        if(option == null) option = "";
+        setValue(option);
+    }
+
     return(
-        <div>
-            {data}
-        </div>
+        <>
+            <div>
+                <button onClick={() => changeDataOutput("")}>All</button>
+                <button onClick={() => changeDataOutput("/1")}>1</button>
+                <button onClick={() => changeDataOutput("/2")}>2</button>
+                <button onClick={() => changeDataOutput("/3")}>3</button>
+                <button onClick={() => changeDataOutput("/4")}>4</button>
+                <button onClick={() => changeDataOutput("/5")}>5</button>
+            </div>
+            <div>
+                Ergebnis in die Konsole!
+            </div>
+            <div>
+                Ergebnislänge: {data.length == 5 ? 5 : 1}
+            </div>
+        </>
     );
 }
