8986b7f38761d676d5552e41f77c7a32fcf169fd
RW <rw@istec.de>
2023-02-17 09:46:33 +0100

    mvn build in workflow eingebaut

--- backend/src/main/java/de/istec/GreetingResource.java
+++ backend/src/main/java/de/istec/GreetingResource.java
@@ -1,19 +1,15 @@
 package de.istec;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 @Path("/api")
 public class GreetingResource {
 
-    @Inject
-    EntityManager em;
+    //@Inject
+    //EntityManager em;
 
     @GET
     @Path("/hello")
@@ -25,22 +21,22 @@ public class GreetingResource {
 
 
 
-    @GET
-    @Path("/data")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<de.istec.Data> data() {
-
-        return em.createNamedQuery("getAllData", Data.class).getResultList();
-    }
-
-    @GET
-    @Path("/data/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Data byId(@PathParam("id") Long id) {
-
-        return (Data) em.createNativeQuery(
-                "SELECT id, name, devmode, somenumber FROM data d where id = :id", Data.class)
-                .setParameter("id", id)
-                .getSingleResult();
-    }
+//    @GET
+//    @Path("/data")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<de.istec.Data> data() {
+//
+//        return em.createNamedQuery("getAllData", Data.class).getResultList();
+//    }
+//
+//    @GET
+//    @Path("/data/{id}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Data byId(@PathParam("id") Long id) {
+//
+//        return (Data) em.createNativeQuery(
+//                "SELECT id, name, devmode, somenumber FROM data d where id = :id", Data.class)
+//                .setParameter("id", id)
+//                .getSingleResult();
+//    }
 }
